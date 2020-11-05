ef09928f3b158adc1369909c1019d24e937252d5
yobleck <sennewell@gmail.com>
2020-11-04 20:27:29 -0500

    fixed verbosity argument
    should now work regardless of number of previous arguments

--- running.py
+++ running.py
@@ -1,8 +1,13 @@
 import telnetlib, json, sys;
 
+verbose = False;
+if(str(sys.argv[-1]) in ["v", "-v", "-verbose", "--verbose"]):  #verbosity must be last argument
+    verbose = True;
+    sys.argv.pop(); #remove verbosity argument to simplify nested if statements below
+
 if(len(sys.argv) > 1):
     if(len(sys.argv) > 2):
-        tn = telnetlib.Telnet(str(sys.argv[1]), sys.argv[2]); #user inputs ip address and port
+        tn = telnetlib.Telnet(str(sys.argv[1]), sys.argv[2]); #user inputs ip and port
     else:
         tn = telnetlib.Telnet(str(sys.argv[1]), 36330); #user inputs ip only
 else:
@@ -16,7 +21,7 @@ while(True): #reading line by line bypasses all the fluff at the beginning and e
     #print("line:", i, line);
     
     if(b"id" in line): #TODO: case where there are multiple work units
-        out = json.loads( line.strip(b"\n") );
+        out = json.loads( line.strip(b"\n") ); #read info from telnet and convert to dict
     if(b"basecredit" in line): #exit if 1 work unit running TODO: does this work with >1 WU
         break;
     if(b"---" in line): #exit if no WUs running
@@ -26,7 +31,7 @@ while(True): #reading line by line bypasses all the fluff at the beginning and e
 tn.close();
 
 if("out" in locals()):
-    if(str(sys.argv[-1]) in ["v", "-v", "-verbose", "--verbose"]):
+    if(verbose):
         print(out);
     else:
         print("id:", out["id"], "\npercentdone:", out["percentdone"], "\neta:", out["eta"], "\ncreditestimate:", out["creditestimate"]);
